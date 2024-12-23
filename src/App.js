7a5d7c808e50b9c795fd061ea948e998d815e1bc
Nicole Han <nicolehan@berkeley.edu>
2024-12-22 19:40:34 -0800

    Imported link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Introduction from './pages/introduction';
 import Downloads from './pages/downloads';
 import './App.css';
